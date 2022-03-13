230e041db533544b652e8f73196d4881b2f1804c
kevinstadler <kevinstadler@users.noreply.github.com>
2022-03-13 21:30:15 +0100

    Improve Shadick flashcard extraction and add import note

--- flashcards/Shadick First Course in Literary Chinese/README.md
+++ flashcards/Shadick First Course in Literary Chinese/README.md
@@ -2,6 +2,13 @@
 
 The flashcards were not created by myself, I merely extracted the content of an unattributed German language PDF titled *Klassik Shadick-Vokabeln Chinesisch-Deutsch* that's floating around the web.
 
+If you want the flashcards to test you on the German definitions (and avoid them being merged with already existing dictionary-definition based flashcards) you should do `flashcards > Import/Export > Import Cards` with the following settings:
+
+```
+* Definition source: File Only
+* Duplicate entries: Allow
+```
+
 ```
 pdftotxt -raw shadick.pdf
 pip3 install unidecode
--- flashcards/Shadick First Course in Literary Chinese/extract.py
+++ flashcards/Shadick First Course in Literary Chinese/extract.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import re
 
-t = open('shadick.txt').read()
+t = open('shadick.txt').read().replace('\f', '')
 
 # split cleanly to delete Wortreferenz until chapter headers
 sections = re.split(r'^\f?((?:Übung )?)Lektion ', t, flags=re.MULTILINE)[1:]
@@ -15,12 +15,13 @@ contents = [ c[1].split('Wortreferenz', 1)[0] for c in contents ]
 import unidecode
 # 2nd and 4th tone: https://www.unicode.org/charts/PDF/U0080.pdf
 # 1st and 3rd tone: https://www.unicode.org/charts/PDF/U0100.pdf
-tonechars = "āáăàēéĕèīíĭìōóŏòūúŭù   ǜ"
+tonechars = "āáăàēéĕèīíĭìōóŏòūúŭùǜ"
 # ǒ ŏ
 
-wadegiles = r"[a-zü'1-5\.]+[1-5]"
+wadegiles = r"[A-Za-zü\'1-5\.]+[1-5]"
 # pinyin = r'[!\w]*[a-z' + tonechars + '\.]+' # disallow upper case endings
-pinyin = r'[!\w]*[' + tonechars + ']\w*' # disallow upper case endings …
+pinyinendings = tonechars + 'aeiougnr'
+pinyin = f'\w*[{pinyinendings}]' # disallow upper case endings or ending in …
 hanzi = r'\u4e00-\u9fff'
 
 def removespaces(match):
@@ -32,26 +33,30 @@ def tonenumber(match):
   return unidecode.unidecode(match.group(1)) + match.group(2) + str(tone)
 
 def processcategory(categoryheader, t):
+    # if header is Lektion remove first 3 lines (bogus hanzi)
+    if not 'Ü' in categoryheader:
+      for i in range(3):
+        t = t[t.find('\n')+1:]
 
     # remove single spaces from personal name pinyins
-    t = re.sub('([A-Z]\w*[' + tonechars + ']\w*( \w+)+)', removespaces, t)
+    # t = re.sub('([A-Z]\w*[' + tonechars + ']\w*( \w+)+)', removespaces, t)
 
-    # switch left-column hanzi from second to first line
-    # t = re.sub(f'^\s*(   {wadegiles}\s+{pinyin}.*)\n(\d* ?[{hanzi}].*?)(\s{5}.*)?', r'\2\n\1 \3', t, flags=re.MULTILINE)
-    # one more time for lines that don't have wade-giles
-    # t = re.sub(f'^\s*(   {pinyin}\s*.*)\n(\d* ?[{hanzi}]\S*)(\s{5}.*)?', r'\2\n   dummy1 \1 \3', t, flags=re.MULTILINE)
-
-    # put hanzi at start of line, remove wade giles
-    t = re.sub(f'^(\d* ?[{hanzi}]\S*[{hanzi} ]*)\n[ ]*{wadegiles}\s+({pinyin})', r'\1\t\t\2\t\t', t, flags=re.MULTILINE)
-    # TODO also match 2nd hanzi (which is after 3+ spaces)
+    # hanzi[/ or ...]/\n[. or hanzi]
+    t = re.sub(f'([{hanzi}](?:\.\.\.|/))\n([{hanzi}\.])', r'\1\2', t)
+    # hanzi\nhanzi to space
+    t = re.sub(f'([{hanzi}])\n([{hanzi}])', r'\1 \2', t)
+    # traditional and simplified
+    t = re.sub(f'^(\d* ?)([{hanzi}][\w/]*) ([{hanzi}]\w*)\n[ ]*{wadegiles}\s+({pinyin})', r'\1\3[\2]\t\4\t', t, flags=re.MULTILINE)
+    # traditional only
+    t = re.sub(f'^(\d* ?[{hanzi}](?:/ |\w)*)\n[ ]*(?:{wadegiles}\s+)?({pinyin})', r'\1\t\2\t', t, flags=re.MULTILINE)
 
     # replace tone accents with numbers
-    t = re.sub(f'([{tonechars}])([eiou]?(:?r|ng?)?)', tonenumber, t)
+    t = re.sub(f'([{tonechars}])([aeiou]?(:?r|ng?)?)', tonenumber, t)
     # add sentence subgroups
     t = re.sub(f'^(\d+) ?([{hanzi}])', categoryheader + r'/\1\n\2', t, flags=re.MULTILINE)
 
     # merge definitions onto previous line (remove preceding newline + space(s)
-    t = re.sub(r'\n[\f ]+(.*)', r' \1', t)
+    t = re.sub(f'\n(?!/|[{hanzi}\[\]]+\t)', r' ', t, flags=re.MULTILINE)
     return '\n'.join([categoryheader, t])
 
 sections = [ processcategory(title, content) for title, content in zip(headers, contents) ]
